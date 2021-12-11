1df57f3f4cdfd33bd4de8be6fa50817d6a412d2b
vhdat <vhdat2002@gmail.com>
2021-12-12 00:21:37 +0700

    update sprite in cpeople

--- CPEOPLE.cpp
+++ CPEOPLE.cpp
@@ -1,40 +1,59 @@
 #include"CPEOPLE.h"
 CPEOPLE::CPEOPLE() {
-	mX = 0;
-	mY = 0;
+	mX = 20;
+	mY = 20;
 	mState = true;
+	sprite.push_back("/ \\");
+	sprite.push_back(" |\\");
+	sprite.push_back("\\o ");	
 };
+void CPEOPLE::Draw() {
+	int y = mY;
+	for (int i = 0; i < 3; i++)
+	{
+		GotoXY(mX,y--);
+		cout << sprite[i];
+	}
+}
+void CPEOPLE::deDraw() {
+	int y = mY;
+	for (int i = 0; i < 3; i++)
+	{
+		GotoXY(mX, y--);
+		cout << "   ";
+	}
+}
 void CPEOPLE::Up(int step) {
 	if (mY == BOARD_STARTY) return;
 	GotoXY(mX, mY);
-	cout << " " << endl;
+	deDraw();
 	mY -= step;
 	GotoXY(mX, mY);
-	cout << "Y" << endl;
+	Draw();
 }
 void CPEOPLE::Left(int step){
 	if (mX == BOARD_STARTX) return;
 	GotoXY(mX, mY);
-	cout << " " << endl;
+	deDraw();
 	mX -= step;
 	GotoXY(mX, mY);
-	cout << "Y" << endl;
+	Draw();
 }
 void CPEOPLE::Right(int step){
 	if (mX == BOARD_ENDX) return;
 	GotoXY(mX, mY);
-	cout << " " << endl;
+	deDraw();
 	mX += step;
 	GotoXY(mX, mY);
-	cout << "Y" << endl;
+	Draw();
 }
 void CPEOPLE::Down(int step) {
 	if (mX == BOARD_ENDY) return;
 	GotoXY(mX, mY);
-	cout << " " << endl;
+	deDraw();
 	mY += step;
 	GotoXY(mX, mY);
-	cout << "Y" << endl;
+	Draw();
 
 }
 //bool CPEOPLE::isImpact(const CVEHICLE*&);
--- CPEOPLE.h
+++ CPEOPLE.h
@@ -2,12 +2,16 @@
 #define cppl
 #include"Console.h"
 #include<iostream>
+#include<vector>
 using namespace std;
 class CPEOPLE {
 	int mX, mY;
+	vector<string> sprite;
 	bool mState;
 public:
 	CPEOPLE();
+	void deDraw();
+	void Draw();
 	void Up(int);
 	void Left(int);
 	void Right(int);
